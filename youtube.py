b44912c64544cb360b451701bca888640c160f8a
teagoram <89437055+teagoram@users.noreply.github.com>
2021-09-11 19:46:33 -0600

    check point

--- youtube.py
+++ youtube.py
@@ -1,20 +1,39 @@
 import pygame
-
 pygame.init()
 # create window
+# width, height
 screen = pygame.display.set_mode((800, 600))
 
 
 # title and icon
 pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+# player
+playerIMG = pygame.image.load('spaceship.png')
+playerImg = pygame.transform.scale(playerIMG, (50, 50))
+playerX = 370
+playerY = 480
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
 
 # game loops
 run = True
 while run:
+    screen.fill((215, 150, 0))
+
+    playerY -= 0.1
+    print(playerX)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         else:
             print(event)
 
+    player(playerX, playerY)
+    pygame.display.update()
 
